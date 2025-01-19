6ec5d3d1c30b53962b4dcc21a5776e5dc102ad9a
noahamar <noahamar@gmail.com>
2025-01-18 21:53:23 -0500

    feat: Polish add members modal

--- src/social/components/AddMemberModal/styles.tsx
+++ src/social/components/AddMemberModal/styles.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const FormContainer = styled.div`
-  border-bottom: 1px solid ${({ theme }) => theme.palette.base.shade4};
+  // border-bottom: 1px solid ${({ theme }) => theme.palette.base.shade4};
 `;
