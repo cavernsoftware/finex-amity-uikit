477f00db93d0574dd4d0dfd3f96fafe94b0b5be9
noahamar <noahamar@gmail.com>
2025-01-20 16:30:52 -0500

    chore: Update exports

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
-export { default as AmityUiKitProviderDirect } from './src/core/providers/UiKitProvider';
-export { default as AmityUiKitSocialDirect } from './src/social/pages/Application';
+export { default as AmityUiKitProviderDirect } from '~/core/providers/UiKitProvider';
+export { default as AmityUiKitSocialDirect } from '~/social/pages/Application';
+
+export { default as useAmityUser } from '~/core/hooks/useUser';
+export { useNavigation as useAmityNavigation } from '~/social/providers/NavigationProvider';
+
+export { default as AmityAvatar } from '~/core/components/Avatar';
+export { useSDK as useAmitySDK } from '~/core/hooks/useSDK';
