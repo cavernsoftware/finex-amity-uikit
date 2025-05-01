a31ca9c4996a4b12151ffdad38cfe999d96d0d2f
noahamar <noahamar@gmail.com>
2025-05-01 19:32:58 -0400

    feat: polish spacing

--- src/v4/social/elements/DebtFreeCountdown/DebtFreeCountdown.tsx
+++ src/v4/social/elements/DebtFreeCountdown/DebtFreeCountdown.tsx
@@ -16,7 +16,7 @@ export function DebtFreeCountdown({ user }: DebtFreeCountdownProps) {
 
   return (
     <div className={styles.debtFreeCountdown}>
-      <Typography.BodyMedium className={styles.debtFreeCountdown__title}>
+      <Typography.BodyMedium>
         Debt-Free Countdown
       </Typography.BodyMedium>
       <Typography.BodyBold className={styles.debtFreeCountdown__daysLeft}>
