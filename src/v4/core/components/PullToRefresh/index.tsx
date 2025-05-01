7fa04f384121f02560bef01ccd2340223175d30c
noahamar <noahamar@gmail.com>
2025-05-01 18:15:02 -0400

    feat: refactor PullToRefresh to prevent refreshing when scrolling up normally

--- src/v4/core/components/PullToRefresh/index.tsx
+++ src/v4/core/components/PullToRefresh/index.tsx
@@ -23,6 +23,12 @@ export const PullToRefresh = forwardRef(function (
 ) {
   const touchStartY = useRef(0);
   const [touchDiff, setTouchDiff] = useState(0);
+
+  // FINEX: Add isAtTop function to check if user is at the top of the page
+  const isAtTop = () => {
+    return window.scrollY <= 0;
+  };
+
   return (
     <div
       ref={ref}
@@ -30,17 +36,39 @@ export const PullToRefresh = forwardRef(function (
       className={className}
       data-testid={accessibilityId}
       onDrag={(event) => event.stopPropagation()}
+      // FINEX: Refactor functions below to prevent touch events from being triggered when scrolling up normally
+      // onTouchStart={(event) => {
+      //   touchStartY.current = event.touches[0].clientY;
+      // }}
+      // onTouchMove={(event) => {
+      //   const touchY = event.touches[0].clientY;
+      //   if (touchStartY.current > touchY) return;
+      //   setTouchDiff(Math.min(touchY - touchStartY.current, 100));
+      // }}
+      // onTouchEnd={() => {
+      //   touchStartY.current = 0;
+      //   if (touchDiff >= 75) onTouchEndCallback?.();
+      //   setTouchDiff(0);
+      // }}
+      // FINEX: Refactor onTouchStart
       onTouchStart={(event) => {
+        if (!isAtTop()) return;
         touchStartY.current = event.touches[0].clientY;
       }}
+      // FINEX: Refactor onTouchMove
       onTouchMove={(event) => {
+        if (!isAtTop() || touchStartY.current === 0) return;
         const touchY = event.touches[0].clientY;
-        if (touchStartY.current > touchY) return;
+        if (touchStartY.current > touchY) {
+          setTouchDiff(0);
+          return;
+        }
         setTouchDiff(Math.min(touchY - touchStartY.current, 100));
       }}
+      // FINEX: Refactor onTouchEnd
       onTouchEnd={() => {
+        if (touchDiff >= 75 && isAtTop()) onTouchEndCallback?.();
         touchStartY.current = 0;
-        if (touchDiff >= 75) onTouchEndCallback?.();
         setTouchDiff(0);
       }}
     >
